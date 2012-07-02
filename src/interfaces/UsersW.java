592886c60fe349f8a9d3b621d18fbbb488383458
marcoblbr <marco.antonio.unicamp@gmail.com>
2012-07-02 18:10:37 -0300

    Users Control

--- src/interfaces/UsersW.java
+++ src/interfaces/UsersW.java
@@ -6,9 +6,14 @@ package interfaces;
 	import java.awt.BorderLayout;
 	import java.awt.EventQueue;
 	
+import javax.swing.DefaultListModel;
 	import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 	import javax.swing.JPanel;
 	import javax.swing.border.EmptyBorder;
+import javax.swing.JList;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.event.ListSelectionEvent;
 	
 /**
  * @author rdru
@@ -18,7 +23,9 @@ package interfaces;
 public class UsersW extends JFrame {
 
 	private JPanel contentPane;
-
+	public  DefaultListModel listData = new DefaultListModel();
+	private JList list; 
+	public EMUW emuw; 
 	/**
 	 * Launch the application.
 	 */
@@ -34,17 +41,39 @@ public class UsersW extends JFrame {
 			}
 		});
 	}
+	
+	
 
 	/**
 	 * Create the frame.
 	 */
 	public UsersW() {
-		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		//setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		list = new JList(listData);
+		
+	
+		list.addListSelectionListener(new ListSelectionListener() {
+			public void valueChanged(ListSelectionEvent arg0) {
+		 		// See if this is a listbox selection and the
+		 		// event stream has settled
+					// Get the current selection and place it in the
+					// edit field
+					String stringValue = list.getSelectedValue().toString();
+					emuw.highLight(stringValue);
+		} } );
+		contentPane.add(list, BorderLayout.CENTER);
+	}
+	
+	public void updateUsers()
+	{
+		
 	}
 
 }
